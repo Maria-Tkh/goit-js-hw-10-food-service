8b3e984d95b44b083cc54495114a5a076ae6c1db
Maria-Tkh <mcweaver117@gmail.com>
2021-07-03 20:15:27 +0300

    Homework 10

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,47 +5,29 @@ const Theme = {
 
 const body = document.querySelector('body');
 const themeSwitchToggle = document.querySelector('.theme-switch__toggle');
-// console.log(themeSwitchToggle);
 
-// 1-й способ
-
-// themeSwitchToggle.addEventListener('change', onThemeSwitchToggleChange);
-
-// let isLightThemeAdjusted;
-
-// function onThemeSwitchToggleChange(event) {
+themeSwitchToggle.addEventListener('change', (event) => {
     
-//     if (isLightThemeAdjusted = !event.target.checked) {
-//         body.classList.remove('dark-theme');
-//         body.classList.add('light-theme');
-//        localStorage.setItem('ui', Theme.LIGHT);
-//     } if (isLightThemeAdjusted = event.target.checked) {
-//         //  body.classList.remove('light-theme');
-//         body.classList.add('dark-theme');
-        
-//         localStorage.setItem('ui', Theme.DARK);
-//     }
-//     // console.log(isLightThemeAdjusted);
-// }
-
-// 2-й способ (буль)
-
-let isLightThemeAdjusted = true;
-
-themeSwitchToggle.addEventListener('change', () => {
-
-    if (isLightThemeAdjusted) {
-        //  body.classList.remove('light-theme');
+    if (event.target.checked) {
+        body.classList.remove('light-theme');
         body.classList.add('dark-theme');
         localStorage.setItem('ui', Theme.DARK);
-        
     } else {
         body.classList.remove('dark-theme');
         body.classList.add('light-theme');
         localStorage.setItem('ui', Theme.LIGHT);
-      
     }
-      isLightThemeAdjusted = !isLightThemeAdjusted; 
 });
+
+const currentTheme = localStorage.getItem('ui');
+if (currentTheme) {
+  body.classList.add(currentTheme);
+  if (body.classList.contains(Theme.DARK)) {
+    themeSwitchToggle.checked = true;
+  }
+} else {
+  body.classList.add('light-theme');
+}
+   
 
 
